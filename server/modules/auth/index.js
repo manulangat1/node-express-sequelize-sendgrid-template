093778e903cf81f3a5ff3080a6a319c8169e926e
manulangat <emmanuelthedeveloper@gmail.com>
2021-01-31 20:02:42 +0300

    ft-implementing-login-functionality

--- server/modules/auth/index.js
+++ server/modules/auth/index.js
@@ -4,6 +4,6 @@ import AuthController from './AuthController';
 const router = express.Router()
 
 router.get('/register',AuthController.registerUser).post('/register',AuthController.registerUser)
-
+router.post('/login',AuthController.login)
 
 export default router;
